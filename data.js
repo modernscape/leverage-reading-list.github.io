99d157010061b107fb3566fd556cd517ef49ea48
modernscape <modernscape@gmail.com>
2021-10-17 13:16:45 +0900

    initial

---
+++ data.js
@@ -0,0 +1,121 @@
+var exItems = [
+  {
+    title: 'マンガ版 人生の勝負は、朝で決まる',
+    image: 'B085G3WS93.01._SCLZZZZZZZ_SX500_',
+    url: 'https://www.amazon.co.jp/dp/4054067816?tag=booklogjp-item-22&linkCode=ogi&th=1&psc=1',
+    done: true,
+  }, 
+  {
+    title: '50歳からのゼロ・リセット',
+    image: '51sdWhvsvNL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'マンガでやさしくわかるブルー・オーシャン戦略',
+    image: '61n0CPrnthL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '僕は君たちに武器を配りたい エッセンシャル版',
+    image: '41unUzyYlcL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'メモの魔力',
+    image: '510MVcp7zoL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '最高の教師がマンガで教える勝利のメンタル',
+    image: '51nEbuZrLDL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '「具体⇄抽象」トレーニング 思考力が飛躍的にアップする29問',
+    image: '41Z9c17yQuL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'マンガでわかる 自己肯定感 ~たった30分読むだけで前向きになれる~',
+    image: '51dILa0uuyL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'アドラー式「ストレスフリーな人」の7つのすごい! 仕事術',
+    image: '61w8ds7wKpS',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'マンガ版「ゼロ」なにもない自分に小さなイチを足していく',
+    image: 'B096TB2V76.01._SCLZZZZZZZ_SX500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '日本一わかりやすい 「強みの作り方」の教科書',
+    image: '51XeJnW1mjL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '13歳から分かる! プロフェッショナルの条件 ドラッカー 成果を上げるレッスン',
+    image: '5117E+4s9NL._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: 'やりきる力',
+    image: '51fo+l+ODgS._SL500_',
+    url:'',
+    done: true,
+  },   
+  {
+    title: '金持ちフリーランス 貧乏サラリーマン',
+    image: 'B08NP9NP4D.01._SCLZZZZZZZ_SX500_',
+    url:'',
+    done: true,
+  },  
+  {
+    title: 'マーケターのように生きろ: 「あなたが必要だ」と言われ続ける人の思考と行動',
+    image: '51HVl2ORWBL._SL500_',
+    url:'',
+    done: true,
+  }, 
+  {
+    title: 'スティーブ・ジョブズ 結果に革命を起こす神のスピード仕事術',
+    image: '511EyqPNO1L._SL500_',
+    url:'',
+    done: true,
+  }, 
+  {
+    title: '',
+    image: '',
+    url:'',
+    done: false,
+  }, 
+  {
+    title: '',
+    image: '',
+    url:'',
+    done: false,
+  } 
+ 
+
+
+
+
+
+
+  
+];
+
+
+
